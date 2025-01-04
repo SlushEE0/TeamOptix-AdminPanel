7a4f16ae76c0384ece24f24f3d7f67f2dae15d61
SlushEE0 <neeladem2009@gmail.com>
2025-01-04 11:42:37 -0800

    HITFIX; router

--- src/app/createAccount/page.tsx
+++ src/app/createAccount/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import toast from "react-hot-toast";
 
